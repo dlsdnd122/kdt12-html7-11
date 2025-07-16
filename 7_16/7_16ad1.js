0604de76fb9272470c13ef94011d7b586e6e790f
InWoong <dlsdnd122@naver.com>
2025-07-16 10:42:43 +0900

    7/16 실습 과제1 update commit

--- 7_16/7_16ad1.js
+++ 7_16/7_16ad1.js
@@ -1,15 +1,15 @@
 // 3인 BMI 계산 실습
 function bmiCaculator(name, height, weight) {
-    heightMeter = height * 0.01
-    bmi = weight/(heightMeter * heightMeter);
+    let heightMeter = height * 0.01
+    let bmi = weight/(heightMeter * heightMeter);
     if (bmi < 18.5) {
-        console.log('저체중입니다.')
+        console.log(name + '님의 bmi 수치는' + bmi.toFixed(1) + '이고 저체중입니다.')
     } else if (bmi >= 18.5 && bmi <= 22.9 ) {
-        console.log("정상입니다.")
+        console.log(name + '님의 bmi 수치는' + bmi.toFixed(1) + '이고 정상입니다.')
     } else if (bmi >= 23 && bmi <= 24.9) {
-        console.log("과체중입니다.")
+        console.log(name + '님의 bmi 수치는' + bmi.toFixed(1) + '이고 과체중입니다.')
     } else if (bmi >= 25) {
-        console.log("비만입니다. 살좀 빼세요")
+        console.log(name + '님의 bmi 수치는' + bmi.toFixed(1) + '이고 비만입니다. 살좀 빼세요.')
     } else {
         console.log("정확한 값을 입력해주세요.")
     }
